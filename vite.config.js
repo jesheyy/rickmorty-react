f702cef4ff90898aeb90b1a3d76abb28ab9d5b09
jesheyy <jessicaysheila100@gmail.com>
2024-11-10 15:16:19 +0100

    segundo commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: "https://faztweb.github.io/react-rick-and-morty/",
-  plugins: [react(), eslint()],
-});
+  plugins: [react()],
+})
